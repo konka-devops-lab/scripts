bb48b4fbb00dacbf1da5679b43fcd7714cc73ca3
siva <sivaram0434@gmail.com>
2025-09-10 19:33:32 +0530

    update the script

--- docker-installation.sh
+++ docker-installation.sh
@@ -7,23 +7,8 @@ chmod +x /usr/local/bin/docker-compose
 
 curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sudo sh
 
-[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.30.0/kind-linux-amd64
-chmod +x ./kind
-sudo mv ./kind /usr/local/bin/kind
-
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-chmod +x kubectl
-sudo mv kubectl /usr/local/bin/kubectl
-
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-chmod 700 get_helm.sh
-./get_helm.sh
-
-sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
-sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
-sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
-
-curl -sS https://webinstall.dev/k9s | bash
+git clone https://github.com/konka-devops-lab/scripts.git /home/ec2-user/scripts
+chown -R ec2-user:ec2-user /home/ec2-user/scripts
 
 
 
