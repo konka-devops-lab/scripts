f5c49f846d0ef946fd289e04e7c8cc7a6fd215a9
siva <sivaram0434@gmail.com>
2025-09-22 09:43:00 +0530

    argocd script was updated

--- kind/argocd.sh
+++ kind/argocd.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl create namespace monitoring
 helm install prometheus oci://ghcr.io/prometheus-community/charts/kube-prometheus-stack --namespace monitoring
+
+
