ea3b492a115362091c5104037fbfd41bda7d1654
siva <sivaram0434@gmail.com>
2025-09-23 10:17:54 +0530

    update the fleet role

--- fleet.sh
+++ fleet.sh
@@ -106,7 +106,7 @@ else
   FLEET_CONFIG_FILE="fleet-config.json"
   cat > $FLEET_CONFIG_FILE <<EOF
 {
-  "IamFleetRole": "arn:aws:iam::384570460482:role/all-rounder",
+  "IamFleetRole": "arn:aws:iam::384570460482:role/aws-service-role/spot.amazonaws.com/AWSServiceRoleForEC2Spot",
   "TargetCapacity": 1,
   "AllocationStrategy": "lowestPrice",
   "LaunchSpecifications": [
