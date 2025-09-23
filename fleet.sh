14a5be83ebb7c1b7abc8a3e46896901853054eb0
siva <sivaram0434@gmail.com>
2025-09-23 09:51:45 +0530

    update the script

--- fleet.sh
+++ fleet.sh
@@ -106,7 +106,7 @@ else
   FLEET_CONFIG_FILE="fleet-config.json"
   cat > $FLEET_CONFIG_FILE <<EOF
 {
-  "IamFleetRole": "arn:aws:iam::<YOUR_ACCOUNT_ID>:role/aws-ec2-spot-fleet-role",
+  "IamFleetRole": "arn:aws:iam::384570460482:role/aws-ec2-spot-fleet-role",
   "TargetCapacity": 1,
   "AllocationStrategy": "lowestPrice",
   "LaunchSpecifications": [
