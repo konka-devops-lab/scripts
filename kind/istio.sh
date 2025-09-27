038929e8e1202f8b4a48c8501e5ea9ba76ecc818
siva <sivaram0434@gmail.com>
2025-09-27 20:23:47 +0530

    istio script was updated

--- kind/istio.sh
+++ kind/istio.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 helm repo add istio https://istio-release.storage.googleapis.com/charts
 helm repo update
-helm install istio-base istio/base -n istio-system
-helm install istiod istio/istiod -n istio-system --wait
-helm install istio-ingressgateway istio/gateway -n istio-system
-helm install istio-egressgateway istio/gateway -n istio-system
+helm upgrade --install istio-base istio/base -n istio-system
+helm upgrade --install istiod istio/istiod -n istio-system --wait
+helm upgrade --install istio-ingressgateway istio/gateway -n istio-system
+helm upgrade --install istio-egressgateway istio/gateway -n istio-system
 
 
 echo "Add Kiali Helm repo and install Kiali"
 helm repo add kiali https://kiali.org/helm-charts
 helm repo update
 
-helm install kiali-server kiali/kiali-server \
+helm upgrade --install kiali-server kiali/kiali-server \
   -n istio-system \
   --set auth.strategy="anonymous" \
-  --set external_services.prometheus.url="http://prometheus-kube-prometheus-prometheus.monitoring.svc.cluster.local:9090"
-  --set external_services.grafana.url="http://prometheus-grafana.monitoring.svc.cluster.local:3000"
+  --set external_services.prometheus.url="http://prometheus-kube-prometheus-prometheus.monitoring.svc.cluster.local:9090" \
+  --set external_services.grafana.external_url="http://prometheus-grafana.monitoring.svc.cluster.local:80"
 
 # echo "Add Jaeger Helm repo and install Jaeger"
 
