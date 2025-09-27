141dfb8c1d798140d8c434edd21ce957d36923ea
siva <sivaram0434@gmail.com>
2025-09-27 16:25:53 +0530

    add the kiali helm chart

--- kind/istio.sh
+++ kind/istio.sh
@@ -7,15 +7,15 @@ helm install istio-ingressgateway istio/gateway -n istio-system
 helm install istio-egressgateway istio/gateway -n istio-system
 
 
-# echo "Add Kiali Helm repo and install Kiali"
-# helm repo add kiali https://kiali.org/helm-charts
-# helm repo update
+echo "Add Kiali Helm repo and install Kiali"
+helm repo add kiali https://kiali.org/helm-charts
+helm repo update
 
-# helm install kiali-server kiali/kiali-server \
-#   -n istio-system \
-#   --set auth.strategy="anonymous" \
-#   --set external_services.prometheus.url="http://prometheus-kube-prometheus-prometheus.monitoring.svc.cluster.local:9090"
-#   --set external_services.grafana.url="http://grafana.monitoring.svc.cluster.local:3000"
+helm install kiali-server kiali/kiali-server \
+  -n istio-system \
+  --set auth.strategy="anonymous" \
+  --set external_services.prometheus.url="http://prometheus-kube-prometheus-prometheus.monitoring.svc.cluster.local:9090"
+  --set external_services.grafana.url="http://prometheus-grafana.monitoring.svc.cluster.local:3000"
 
 # echo "Add Jaeger Helm repo and install Jaeger"
 
