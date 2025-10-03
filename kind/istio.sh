a3da16b84b24c1fa969a9d60f2c533c71f3e2721
siva <sivaram0434@gmail.com>
2025-10-03 16:49:47 +0530

    port forward script was updated

--- kind/istio.sh
+++ kind/istio.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 helm repo add istio https://istio-release.storage.googleapis.com/charts
+helm repo add jaegertracing https://jaegertracing.github.io/helm-charts
+helm repo add kiali https://kiali.org/helm-charts
 helm repo update
+kubectl create namespace istio-system
+
 helm upgrade --install istio-base istio/base -n istio-system
 helm upgrade --install istiod istio/istiod -n istio-system --wait
 helm upgrade --install istio-ingressgateway istio/gateway -n istio-system
 helm upgrade --install istio-egressgateway istio/gateway -n istio-system
 
-
-echo "Add Kiali Helm repo and install Kiali"
-helm repo add kiali https://kiali.org/helm-charts
-helm repo update
 
 helm upgrade --install kiali-server kiali/kiali-server \
   -n istio-system \
@@ -18,6 +18,15 @@ helm upgrade --install kiali-server kiali/kiali-server \
   --set external_services.grafana.enabled=true \
   --set external_services.grafana.in_cluster_url="http://prometheus-grafana.monitoring.svc.cluster.local:80" \
   --set external_services.grafana.external_url="http://prometheus-grafana.monitoring.svc.cluster.local:80"
+
+
+helm upgrade --install jaeger jaegertracing/jaeger \
+  --namespace istio-system \
+  --set provisionDataStore.cassandra=false \
+  --set storage.type=memory \
+  --set persistence.enabled=false \
+  --set collector.zipkinHostPort=:9411
+
 
 
 
