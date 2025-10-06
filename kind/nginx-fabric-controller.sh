3077d6ed53dd066d79e89b36908eeffd9a63a33a
siva <sivaram0434@gmail.com>
2025-10-06 20:57:15 +0530

    add the CRDs

--- kind/nginx-fabric-controller.sh
+++ kind/nginx-fabric-controller.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+kubectl kustomize "https://github.com/nginx/nginx-gateway-fabric/config/crd/gateway-api/standard?ref=v2.1.0" | kubectl apply -f -
+
 helm install ngf oci://ghcr.io/nginx/charts/nginx-gateway-fabric \
   --create-namespace -n ngf-gatewayapi-ns
