a99b4e480ad48efb8de5b01c9e471c7e69b1eacd
siva <sivaram0434@gmail.com>
2025-10-11 16:37:23 +0530

    update the port

--- kind/port-forward.sh
+++ kind/port-forward.sh
@@ -6,7 +6,7 @@ commands=(
   "kubectl port-forward svc/nginx-kibana -n logging --address 0.0.0.0 8082:80"
   "kubectl port-forward svc/dev-frontend -n instana --address 0.0.0.0 8083:80"
   "kubectl port-forward svc/kiali -n istio-system --address 0.0.0.0 8084:20001"
-  "kubectl port-forward -n istio-system svc/jaeger-query --address 0.0.0.0 8085:80"
+  "kubectl port-forward -n istio-system svc/jaeger-query --address 0.0.0.0 8087:80"
   "kubectl port-forward -n testing svc/demo-service  --address0.0.0.0 8086:80"
 )
 
